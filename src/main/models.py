69c6d65d97c3422e92dcaa683b2d3992b1996187
danielecollins <57637750+danielecollins@users.noreply.github.com>
2021-10-12 03:03:37 -0600

    created the database table

--- src/main/models.py
+++ src/main/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class groceries(models.Model):
+    item = models.CharField(max_length=100)
+    walmart_price = models.DecimalField(max_digits=5, decimal_places=2)
+    boulims_price = models.DecimalField(max_digits=5, decimal_places=2)
+    albertsons_price = models.DecimalField(max_digits=5, decimal_places=2)
