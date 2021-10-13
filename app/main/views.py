72bf9f19d9a05cb5ab3a288f32d976ea477bb09c
Daryl Appleton <app19002@byui.edu>
2021-10-13 13:16:13 -0600

    fixed the db query

--- app/main/views.py
+++ app/main/views.py
@@ -10,7 +10,7 @@ def index(request):
     return HttpResponse("Hello World!")
 
 def book_by_id(request, book_id):
-    book = Book.objects.get(pk=book_id)
+    book = groceries.objects.get(pk=book_id)
     return render(request, 'book_details.html', {'book':book})
 
 class HomePageView(TemplateView):
