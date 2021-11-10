ecd5228740582c1e2b00545667705c2349ece880
danielecollins <57637750+danielecollins@users.noreply.github.com>
2021-11-10 12:44:32 -0700

    Change you guys can present

--- app/main/views.py
+++ app/main/views.py
@@ -10,7 +10,7 @@ def grocery_by_id(request, grocery_id):
     return render(request, 'grocery_details.html', {'grocery':grocery})
 
 def add_grocery(request):
-    item = request.GET.get('q')
+    new_item = request.GET.get('q')
     
     # This will update or add grocerys if you want
     if request.method == "POST":
@@ -18,21 +18,23 @@ def add_grocery(request):
         new_broulims_price = request.POST.get('broulims_price')
         new_albertsons_price = request.POST.get('albertsons_price')
 
-        # if request.POST.get('owned') is None:
-        #     new_owned = False
-        # else:
-        #     new_owned = request.POST.get('owned')
+        if request.POST.get('owned') is None:
+            new_owned = False
+        else:
+            new_owned = request.POST.get('owned')
         
-        # if Stock.objects.filter(ticker=new_ticker).exists():
-        #     Stock.objects.filter(ticker=new_ticker).update(ask=new_ask)
-        #     Stock.objects.filter(ticker=new_ticker).update(bid=new_bid)
-        #     Stock.objects.filter(ticker=new_ticker).update(owned=new_owned)
-        # else:
-        #     s = Stock(ticker=new_ticker, ask=new_ask, bid=new_bid, owned=new_owned)
-        #     s.save()
+        grocery = groceries.objects.filter(item=new_item)
+
+        if grocery.exists():
+            grocery.update(walmart_price=new_walmart_price)
+            grocery.update(broulims_price=new_broulims_price)
+            grocery.update(albertsons_price=new_albertsons_price)
+        else:
+            s = groceries(item=new_item, walmart_price=new_walmart_price, broulims_price=new_broulims_price, albertsons_price=new_albertsons_price)
+            s.save()
 
     context = {
-        'item':item
+        'item':new_item
     }
     return render(request, "add_grocery.html", context)
 
