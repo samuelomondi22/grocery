f06de959615b1904184e2d950a18bc43bc3e4938
samuelomondi22 <omondisam696@gmail.com>
2021-11-15 13:08:08 -0700

    twomodels

--- app/main/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Created the groceries table to hold the groceries
-class groceries(models.Model):
-    item = models.CharField(max_length=100)
-    walmart_price = models.DecimalField(max_digits=5, decimal_places=2)
-    broulims_price = models.DecimalField(max_digits=5, decimal_places=2)
-    albertsons_price = models.DecimalField(max_digits=5, decimal_places=2)
--- app/main/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import groceries
-
-
-class TodoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = groceries
-        fields = "__all__"
